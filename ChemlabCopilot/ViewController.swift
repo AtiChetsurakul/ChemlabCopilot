4b4f89d3185428c785baa317668208fb9016368c
atiChetsurakul <ksde34@gmail.com>
2021-12-06 14:24:11 +0700

    Initial Commit

---
+++ ChemlabCopilot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChemlabCopilot
+//
+//  Created by ati chetsurakul on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
